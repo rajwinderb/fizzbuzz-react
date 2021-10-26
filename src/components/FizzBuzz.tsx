9b658d6cf29b32bed6717b7aa497872c60862eda
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2021-10-26 14:45:58 +0100

    initial commit

---
+++ src/components/FizzBuzz.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function FizzBuzz(): JSX.Element {
+  return (
+    <>
+      <h1>Fizz Buzz Generator</h1>
+      <hr />
+      <p>Fizz Buzz Sequence: 1</p>
+      <hr />
+      <button>NEXT</button>
+    </>
+  );
+}
